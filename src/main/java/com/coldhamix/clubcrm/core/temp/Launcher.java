0db1162fe61bb435ba3b8e12f58c62ed83c3fbaa
Vadim A. Khamzin <midav123@yandex.ru>
2016-01-23 01:23:08 +0400

    Initial commit

---
+++ src/main/java/com/coldhamix/clubcrm/core/temp/Launcher.java
@@ -0,0 +1,14 @@
+package com.coldhamix.clubcrm.core.temp;
+
+/**
+ * Copyright 2016 (c) ClubCRM.
+ * Author: Vadim A. Khamzin.
+ * Created on: 23-01-2016
+ */
+public class Launcher {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, CRM!");
+    }
+
+}
